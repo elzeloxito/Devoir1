0efdef39153e1a6cffb08865af0f2817b23a199e
elzeloxito <theo.piliszczukwild@gmail.com>
2026-01-19 21:45:58 -0500

    Fichiers DEVOIR

--- main.py
+++
@@ -1,7 +0,0 @@
-print('Pizza !')
-
-print('Pasta')
-
-#test Axel one more modification
-#si tu vois ce message c'est que j'arrive à synchroniser et toi à récuppérer les fichiers
-#je fais un dernier test
